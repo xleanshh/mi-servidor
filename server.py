a94a790589b54d7228219d38b37e3e131c180e0d
xleanshh <xleanshh@gmail.com>
2024-08-11 06:46:51 +0000

    Mundo guardado 2024-08-11

--- server.py
+++ server.py
@@ -7,7 +7,7 @@
 
 # Regiones de ngrok
 # Código          Lugar
-#-----------      ---------------------------
+#-----------      --------------------------- 
 # ap	          Asia / Pacífico (Singapore)
 # au		      Australia (Sydney)
 # eu		      Europa (Frankfurt)
